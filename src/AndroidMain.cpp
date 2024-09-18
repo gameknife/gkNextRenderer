2f6ff0e063d5f7298a50d93faf585b832fe841d1
gameKnife <kaimingyi@163.com>
2024-09-18 23:27:46 +0800

    Divide Android and Desktop main

---
+++ src/AndroidMain.cpp
@@ -0,0 +1,136 @@
+#include "Vulkan/Enumerate.hpp"
+#include "Vulkan/Strings.hpp"
+#include "Utilities/Console.hpp"
+#include "Utilities/Exception.hpp"
+#include "Options.hpp"
+#include "Runtime/Application.hpp"
+#include <algorithm>
+#include <cstdlib>
+#include <fmt/format.h>
+#include <iostream>
+#include <filesystem>
+
+#include "Runtime/Platform/PlatformCommon.h"
+
+#include <imgui_impl_android.h>
+#include <android/log.h>
+#include <game-activity/native_app_glue/android_native_app_glue.h>
+
+std::unique_ptr<NextRendererApplication> GApplication = nullptr;
+
+void handle_cmd(android_app* app, int32_t cmd) {
+    switch (cmd) {
+    case APP_CMD_INIT_WINDOW:
+        // The window is being shown, get it ready.
+        {
+            MakeExternalDirectory(app, "assets/fonts");
+            MakeExternalDirectory(app, "assets/models");
+            MakeExternalDirectory(app, "assets/shaders");
+            MakeExternalDirectory(app, "assets/textures");
+            MakeExternalDirectory(app, "assets/locale");
+            
+            const char* argv[] = { "gkNextRenderer", "--renderer=4", "--scene=1", "--load-scene=qx50.glb"};
+            const Options options(4, argv);
+            GOption = &options;
+            GApplication.reset(new NextRendererApplication(options));
+            __android_log_print(ANDROID_LOG_INFO, "vkdemo",
+                "start gknextrenderer: %d", options.RendererType);
+            GApplication->Start();
+        }
+        break;
+    case APP_CMD_TERM_WINDOW:
+        // The window is being hidden or closed, clean it up.
+        {
+            
+        }
+        break;
+    default:
+        __android_log_print(ANDROID_LOG_INFO, "Vulkan Tutorials",
+                            "event not handled: %d", cmd);
+    }
+}
+
+static int32_t engine_handle_input(struct android_app* app) {
+    ImGuiIO& io = ImGui::GetIO();
+    //auto* engine = (struct engine*)app->userData;
+    auto ib = android_app_swap_input_buffers(app);
+    if (ib && ib->motionEventsCount) {
+        for (int i = 0; i < ib->motionEventsCount; i++) {
+            auto *event = &ib->motionEvents[i];
+            int32_t ptrIdx = 0;
+            switch (event->action & AMOTION_EVENT_ACTION_MASK) {
+            case AMOTION_EVENT_ACTION_POINTER_DOWN:
+            case AMOTION_EVENT_ACTION_POINTER_UP:
+                // Retrieve the index for the starting and the ending of any secondary pointers
+                ptrIdx = (event->action & AMOTION_EVENT_ACTION_POINTER_INDEX_MASK) >>
+                         AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
+            case AMOTION_EVENT_ACTION_DOWN:
+            case AMOTION_EVENT_ACTION_UP:
+                io.AddMouseSourceEvent(ImGuiMouseSource_Mouse);
+                io.AddMousePosEvent(GameActivityPointerAxes_getAxisValue(
+                &event->pointers[ptrIdx], AMOTION_EVENT_AXIS_X) * 0.75, GameActivityPointerAxes_getAxisValue(
+            &event->pointers[ptrIdx], AMOTION_EVENT_AXIS_Y) * 0.75);
+                io.AddMouseButtonEvent(0, event->action == AMOTION_EVENT_ACTION_DOWN);
+
+                GApplication->OnTouch(event->action == AMOTION_EVENT_ACTION_DOWN, GameActivityPointerAxes_getAxisValue(
+                        &event->pointers[ptrIdx], AMOTION_EVENT_AXIS_Y) * 0.75,
+                        GameActivityPointerAxes_getAxisValue(
+                    &event->pointers[ptrIdx], AMOTION_EVENT_AXIS_X) * 0.75);
+
+                break;
+            case AMOTION_EVENT_ACTION_MOVE:
+                // Process the move action: the new coordinates for all active touch pointers
+                // are inside the event->pointers[]. Compare with our internally saved
+                // coordinates to find out which pointers are actually moved. Note that there is
+                // no index embedded inside event->action for AMOTION_EVENT_ACTION_MOVE (there
+                // might be multiple pointers moved at the same time).
+                //...
+                    io.AddMouseSourceEvent(ImGuiMouseSource_Mouse);
+                    io.AddMousePosEvent(GameActivityPointerAxes_getAxisValue(
+                        &event->pointers[ptrIdx], AMOTION_EVENT_AXIS_X) * 0.75,
+                        GameActivityPointerAxes_getAxisValue(
+                    &event->pointers[ptrIdx], AMOTION_EVENT_AXIS_Y) * 0.75);
+
+                GApplication->OnTouchMove(GameActivityPointerAxes_getAxisValue(
+                        &event->pointers[ptrIdx], AMOTION_EVENT_AXIS_Y) * 0.75,
+                        GameActivityPointerAxes_getAxisValue(
+                    &event->pointers[ptrIdx], AMOTION_EVENT_AXIS_X) * 0.75);
+               break;
+            }
+        }
+        android_app_clear_motion_events(ib);
+    }
+
+    // Process the KeyEvent in a similar way.
+    //...
+
+return 0;
+}
+
+void android_main(struct android_app* app)
+{
+    std::cout.rdbuf(new androidbuf);
+    
+    app->onAppCmd = handle_cmd;
+
+    // Used to poll the events in the main loop
+    int events;
+    android_poll_source* source;
+
+    // Main loop
+    do {
+        if (ALooper_pollAll(GApplication != nullptr ? 1 : 0, nullptr,
+                            &events, (void**)&source) >= 0) {
+            if (source != NULL) source->process(app, source);
+                            }
+        
+        engine_handle_input(app);
+        
+        // render if vulkan is ready
+        if (GApplication != nullptr) {
+            GApplication->Tick();
+        }
+    } while (app->destroyRequested == 0);
+
+    delete std::cout.rdbuf(0);
+}
