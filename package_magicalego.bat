f0a3d0997c7df0dccf36d5de9546acab15a18ef0
gameKnife <kaimingyi@163.com>
2024-11-26 16:53:17 +0800

    With MagicaLego Release Script

--- package_magicalego.bat
+++ package_magicalego.bat
@@ -1,9 +1,5 @@
 @echo off
 
-IF "%1" == "clean" (
-    rmdir /S /Q build\windows
-)
-
 call ./build_windows_magicalego.bat
 pushd %CD%
 cd build/windows/bin
@@ -13,6 +9,6 @@ popd
 
 pushd %CD%
 cd build/windows
-tar -a -cf MagicaLego-windows.zip ./bin/MagicaLego.exe ./bin/ffmpeg.exe ./bin/vulkan-1.dll ./assets/legos ./assets/sfx ./assets/paks ./assets/locale ./assets/fonts ./assets/models/legobricks.glb
-move /Y MagicaLego-windows.zip ..\..\
+tar -a -cf "MagicaLego_win64_%1.zip" ./bin/MagicaLego.exe ./bin/ffmpeg.exe ./bin/vulkan-1.dll ./assets/legos ./assets/sfx ./assets/paks ./assets/locale ./assets/fonts ./assets/models/legobricks.glb
+move /Y "MagicaLego_win64_%1.zip" ..\..\
 popd
