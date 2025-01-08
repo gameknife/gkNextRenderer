271055429d25e0c1bc1af58aa034168435c019ee
gameKnife <kaimingyi@163.com>
2025-01-09 00:14:30 +0800

    add animation for proc scene
    - add pbr scene

--- src/Runtime/SceneList.cpp
+++ src/Runtime/SceneList.cpp
@@ -32,19 +32,35 @@ namespace
     }
 
     void AddRayTracingInOneWeekendCommonScene(std::vector<std::shared_ptr<Assets::Node>>& nodes, std::vector<Assets::Model>& models, std::vector<Assets::FMaterial>& materials,
-                                              const bool& isProc, std::function<float ()>& random)
+                                              std::vector<Assets::AnimationTrack>& tracks, std::function<float ()>& random)
     {
         models.push_back(Model::CreateSphere(vec3(0,0,0), 0.2f));
         uint32_t meshIdx = static_cast<uint32_t>(models.size() - 1);
-        for (int i = -11; i < 11; ++i)
+        for (int i = -22; i < 22; ++i)
         {
-            for (int j = -11; j < 11; ++j)
+            for (int j = -22; j < 22; ++j)
             {
                 const float chooseMat = random();
                 const float center_y = static_cast<float>(j) + 0.9f * random();
                 const float center_x = static_cast<float>(i) + 0.9f * random();
                 const vec3 center(center_x, 0.2f, center_y);
 
+                const std::string Name = Utilities::NameHelper::RandomName(6);
+
+                const float Duration = 0.3f;
+                Assets::AnimationTrack track{};
+                track.Duration_ = Duration;
+                track.NodeName_ = Name;
+                int offset = static_cast<int>(random() * 9.0f);
+                for ( int i = 0; i < 11; ++i )
+                {
+                    float time = i / 10.0f;
+                    float shifttime = ((i + offset) % 10) / 10.0f;
+                    float mixer = glm::abs(shifttime - 0.5f) / 0.5f;
+                    track.TranslationChannel.Keys.push_back( {time * Duration, center + vec3(0,glm::sqrt(mixer) * 0.15f,0)} );
+                }
+                tracks.push_back(track);
+                
                 if (length(center - vec3(4, 0.2f, 0)) > 0.9f)
                 {
                     if (chooseMat < 0.7f) // Diffuse
@@ -53,7 +69,7 @@ namespace
                         const float g = random() * random();
                         const float r = random() * random();
                         uint32_t matId = CreateMaterial(materials, Material::Lambertian(vec3(r,g,b)));
-                        nodes.push_back(Assets::Node::CreateNode(Utilities::NameHelper::RandomName(6),
+                        nodes.push_back(Assets::Node::CreateNode(Name,
                                                                  center,
                                                                  quat(1, 0, 0, 0),
                                                                  vec3(1, 1, 1),
@@ -70,7 +86,7 @@ namespace
                         const float g = 0.5f * (1 + random());
                         const float r = 0.5f * (1 + random());
                         uint32_t matId = CreateMaterial(materials, Material::Metallic(vec3(r,g,b), fuzziness));
-                        nodes.push_back(Assets::Node::CreateNode(Utilities::NameHelper::RandomName(6),
+                        nodes.push_back(Assets::Node::CreateNode(Name,
                                                                  center,
                                                                  quat(1, 0, 0, 0),
                                                                  vec3(1, 1, 1),
@@ -84,7 +100,7 @@ namespace
                     {
                         const float fuzziness = 0.5f * random();
                         uint32_t matId = CreateMaterial(materials, Material::Dielectric(1.5f, fuzziness));
-                        nodes.push_back(Assets::Node::CreateNode(Utilities::NameHelper::RandomName(6),
+                        nodes.push_back(Assets::Node::CreateNode(Name,
                                                                  center,
                                                                  quat(1, 0, 0, 0),
                                                                  vec3(1, 1, 1),
@@ -129,11 +145,11 @@ namespace
         nodes.back()->SetVisible(true);
         nodes.back()->SetMaterial({0});
         
-        AddRayTracingInOneWeekendCommonScene(nodes, models, materials, isProc, random);
+        AddRayTracingInOneWeekendCommonScene(nodes, models, materials, tracks, random);
 
         uint32_t matIdx0 = CreateMaterial(materials, Material::Dielectric(1.5f, 0.0f));
         uint32_t matIdx1 = CreateMaterial(materials, Material::Lambertian(vec3(0.4f, 0.2f, 0.1f)));
-        uint32_t matIdx2 = CreateMaterial(materials, Material::Metallic(vec3(0.7f, 0.6f, 0.5f), 0.5f));
+        uint32_t matIdx2 = CreateMaterial(materials, Material::Metallic(vec3(0.7f, 0.6f, 0.5f), 0.1f));
         models.push_back(Model::CreateSphere(vec3(0, 0, 0), 1.0f));
         uint32_t modelIdx = static_cast<uint32_t>(models.size() - 1);
         
@@ -176,14 +192,26 @@ namespace
         models.push_back(box0);
         auto ball0 = Model::CreateSphere(vec3(0, 0, 0), 100.0f);
         models.push_back(ball0);
-        nodes.push_back(Assets::Node::CreateNode(Utilities::NameHelper::RandomName(6), vec3(0 + 130 - 0, 101, 80), quat(vec3(0, 0.5f, 0)), vec3(1, 1, 1), cbox_model + 2, static_cast<uint32_t>(nodes.size()),
+        nodes.push_back(Assets::Node::CreateNode("Sphere1", vec3(130, 101, 80), quat(vec3(0, 0.5f, 0)), vec3(1, 1, 1), cbox_model + 2, static_cast<uint32_t>(nodes.size()),
                                                  false));
         nodes.back()->SetVisible(true);
         nodes.back()->SetMaterial({5});
-        nodes.push_back(Assets::Node::CreateNode(Utilities::NameHelper::RandomName(6), vec3(0 - 130 - 0, 0, -80), quat(vec3(0, 0.25f, 0)), vec3(1, 2, 1), cbox_model + 1, static_cast<uint32_t>(nodes.size()),
+        nodes.push_back(Assets::Node::CreateNode("Box", vec3(-130, 0, -80), quat(vec3(0, 0.25f, 0)), vec3(1, 2, 1), cbox_model + 1, static_cast<uint32_t>(nodes.size()),
                                                  false));
         nodes.back()->SetMaterial({4});
         nodes.back()->SetVisible(true);
+
+        Assets::AnimationTrack track{};
+        track.Duration_ = 1.0f;
+        track.NodeName_ = "Sphere1";
+        for ( int i = 0; i < 11; ++i )
+        {
+            float time = i / 10.0f;
+            float mixer = glm::abs(time - 0.5f) / 0.5f;
+            track.TranslationChannel.Keys.push_back( {time, vec3(130, glm::mix(360, 101, mixer * mixer), 80)} );
+        }
+        
+        tracks.push_back(track);
     }
 }
 
