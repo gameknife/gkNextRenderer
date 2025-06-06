ef626091839c3862b8cde886203ba15e88756b9f
gameKnife <kaimingyi@163.com>
2025-06-06 18:10:00 +0800

    Shader Build System Update

--- build_windows.bat
+++ build_windows.bat
@@ -31,7 +31,7 @@ call :SETTIMER
 
 %CMAKE% -D WITH_OIDN=0 -D WITH_AVIF=0 -D WITH_SUPERLUMINAL=0 -D VCPKG_TARGET_TRIPLET=x64-windows-static -D CMAKE_TOOLCHAIN_FILE=../vcpkg.windows/scripts/buildsystems/vcpkg.cmake -G "Visual Studio 17 2022" -A "x64" ../.. || goto :error
 REM %MSBUILD% gkNextRenderer.sln /p:Configuration=Release /verbosity:minimal /nologo || goto :error
-%CMAKE% --build . -j12
+%CMAKE% --build . --config Release -j12
 
 call :STOPTIMER "Build"
 
