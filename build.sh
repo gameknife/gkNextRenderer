e39666c96d76fc3b8ec3e3f0acd6e543f56794a7
gameKnife <kaimingyi@163.com>
2026-01-14 00:17:01 +0800

    re-fix

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$SCRIPT_DIR"
 
 export VCPKG_ROOT="$PROJECT_ROOT/.vcpkg"
-export VCPKG_BINARY_SOURCES="clear;files,$$PROJECT_ROOT/.vcpkg_bincache,readwrite"
+export VCPKG_BINARY_SOURCES="clear;files,$PROJECT_ROOT/.vcpkg_bincache,readwrite"
 
 # Colors
 GREEN='\033[0;32m'
