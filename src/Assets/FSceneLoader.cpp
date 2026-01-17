3d639ff76df0ca0a78f67b09780ed5a83c87701f
gameKnife <kaimingyi@163.com>
2026-01-17 19:52:53 +0800

    right parse the emmsive
    - emmsive

--- src/Assets/FSceneLoader.cpp
+++ src/Assets/FSceneLoader.cpp
@@ -416,6 +416,7 @@ namespace Assets
             m.DiffuseTextureId = -1;
             m.MRATextureId = -1;
             m.NormalTextureId = -1;
+            m.EmissiveTextureId = -1;
             m.NormalTextureScale = 1.0f;
 
             m.MaterialModel = Material::Enum::Mixture;
@@ -494,21 +495,20 @@ namespace Assets
             }
 
             auto emissive = mat.extensions.find("KHR_materials_emissive_strength");
+            // TODO: may impl per pixel mat type
             if (emissive != mat.extensions.end())
             {
                 float power = static_cast<float>(emissive->second.Get("emissiveStrength").GetNumberAsDouble());
-                m = Material::DiffuseLight(emissiveColor * power * 100.0f);
                 if (mat.emissiveTexture.index != -1)
                 {
-                    m.DiffuseTextureId = lambdaGetTexture(mat.emissiveTexture.index);
+                    m.EmissiveTextureId = lambdaGetTexture(mat.emissiveTexture.index);
                 }
             }
             else if (glm::length(emissiveColor) > 0.0f)
             {
-                m = Material::DiffuseLight(emissiveColor * 5.0f);
                 if (mat.emissiveTexture.index != -1)
                 {
-                    m.DiffuseTextureId = lambdaGetTexture(mat.emissiveTexture.index);
+                    m.EmissiveTextureId = lambdaGetTexture(mat.emissiveTexture.index);
                 }
             }
 
