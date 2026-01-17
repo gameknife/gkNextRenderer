80bb4f3cded9f544d200de14e313684f21a9b927
gameKnife <kaimingyi@163.com>
2026-01-17 22:45:40 +0800

    kinematic setup with node tree

--- src/Assets/Scene.cpp
+++ src/Assets/Scene.cpp
@@ -101,10 +101,20 @@ namespace Assets
         cpuAccelerationStructure_.InitBVH(*this);
 
         // force static flag
+        std::function<void(Node*)> SetKinematicRecursive = [&](Node* node)
+        {
+            if (node == nullptr) return;
+            node->SetMobility(Node::ENodeMobility::Kinematic);
+            for (auto& child : node->Children())
+            {
+                SetKinematicRecursive(child.get());
+            }
+        };
+
         for ( auto& track : tracks_ )
         {
             Node* node = GetNode(track.NodeName_);
-            if (node != nullptr) node->SetMobility(Node::ENodeMobility::Kinematic);
+            SetKinematicRecursive(node);
         }
         
         // calculate the scene aabb
@@ -389,9 +399,13 @@ namespace Assets
             {
                 if (!track.Playing()) continue;
                 track.Time_ += deltaSeconds * track.PlaySpeed_;
-                if (track.Time_ > durationMax || track.Time_ < 0.0f)
+                if (track.Time_ > durationMax)
                 {
-                    track.PlaySpeed_ *= -1.0f;
+                    track.PlaySpeed_ = -1.0f;
+                }
+                if ( track.Time_ < 0.0f )
+                {
+                    track.PlaySpeed_ = 1.0f;
                 }
                 Node* node = GetNode(track.NodeName_);
                 if (node)
@@ -410,7 +424,21 @@ namespace Assets
                     MarkDirty();
 
                     // to physicSys
-                    NextEngine::GetInstance()->GetPhysicsEngine()->MoveKinematicBody(node->GetPhysicsBody(), translation, rotation, 0.01f);
+                    std::function<void(Node*)> UpdatePhysicsBodyRecursive = [&](Node* n)
+                    {
+                        if (!n) return;
+                        JPH::BodyID bodyID = n->GetPhysicsBody();
+                        if (!bodyID.IsInvalid())
+                        {
+                            NextEngine::GetInstance()->GetPhysicsEngine()->MoveKinematicBody(bodyID, n->WorldTranslation(), n->WorldRotation(), 0.01f);
+                        }
+
+                        for (auto& child : n->Children())
+                        {
+                            UpdatePhysicsBodyRecursive(child.get());
+                        }
+                    };
+                    UpdatePhysicsBodyRecursive(node);
 
                     // temporal if camera node, request override
                     if (node->GetName() == "Shot.BlueCar")
