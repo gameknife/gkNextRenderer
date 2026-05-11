3933d4a53f663beb9c6fb31092007d915a4234ce
gameKnife <kaimingyi@163.com>
2026-05-11 20:43:11 +0800

    fix for android build

--- gnb.bat
+++ gnb.bat
@@ -2,14 +2,13 @@
 setlocal
 set "ROOT=%~dp0"
 set "GNB=%ROOT%tools\gnb-bin\windows-amd64\gnb.exe"
-if exist "%ROOT%gnb.exe" set "GNB=%ROOT%gnb.exe"
-if not exist "%GNB%" (
-  if exist "%ProgramFiles%\Go\bin\go.exe" (
-    pushd "%ROOT%tools\gnb"
-    "%ProgramFiles%\Go\bin\go.exe" build -trimpath -ldflags "-s -w" -o "%ROOT%gnb.exe" .\cmd\gnb
-    popd
-    set "GNB=%ROOT%gnb.exe"
-  )
+if exist "%ROOT%gnb.exe" (
+  set "GNB=%ROOT%gnb.exe"
+) else if exist "%ROOT%tools\gnb\go.mod" if exist "%ProgramFiles%\Go\bin\go.exe" (
+  pushd "%ROOT%tools\gnb"
+  "%ProgramFiles%\Go\bin\go.exe" build -trimpath -ldflags "-s -w" -o "%ROOT%gnb.exe" .\cmd\gnb
+  popd
+  set "GNB=%ROOT%gnb.exe"
 )
 if not exist "%GNB%" (
   mkdir "%ROOT%tools\gnb-bin\windows-amd64" >nul 2>nul
